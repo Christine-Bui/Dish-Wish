16d3075f36a13b5ed5baa584ee0f357341cc02a8
Christine Bui <kennen0246@gmail.com>
2024-01-15 15:49:00 -0800

    changes to settings

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_1/pages/collection.dart';
 import 'package:flutter_application_1/pages/home.dart';
 import 'package:flutter_application_1/pages/splash_screen.dart';
+import 'package:flutter_application_1/pages/settings.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const SplashScreen(),
+      home: Settings(),
       theme: ThemeData(
         textTheme:
             GoogleFonts.jetBrainsMonoTextTheme(Theme.of(context).textTheme),
--- flutter_application_1/lib/pages/settings.dart
+++ flutter_application_1/lib/pages/settings.dart
@@ -1,17 +1,115 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/models/recipe_model.dart';
 
-class More extends StatelessWidget {
-  const More({super.key});
+class Settings extends StatefulWidget {
+  const Settings({super.key});
+  @override
+    State<Settings> createState() => _Settings();
+  }
+
+class _Settings extends State<Settings> {
+  List<RecipeModel> recipes = [];
+  void getInitialInfo() {
+    recipes = RecipeModel.getRecipes();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Settings Page',
-        home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Settings'),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Popular Recipes',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  // Add your "See All" button action here
+                },
+                child: Text(
+                  'See All',
+                  style: TextStyle(
+                    color: Colors.blue, // Change color as needed
+                    fontSize: 14,
+                  ),
+                ),
+              ),
+            ],
           ),
-          body: const Text("Settings Page"),
-        ));
+        ),
+        SizedBox(
+          height: 15,
+        ),
+        Container(
+          height: 330,
+          child: ListView.separated(
+            itemBuilder: (context, index) {
+              return Container(
+                width: 238,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.1), // Shadow color
+                      blurRadius: 8, // Blur radius
+                      offset: Offset(3, 6), // Offset in x and y axis
+                    ),
+                  ],
+                ),
+                padding: EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child: SizedBox.fromSize(
+                        size: Size.fromRadius(108),
+                        child: Image.asset(
+                          recipes[index].iconPath,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Padding(padding: const EdgeInsets.only(top: 10)),
+                    Text(
+                      recipes[index].name,
+                      style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        color: Colors.black,
+                        fontSize: 16,
+                      ),
+                    ),
+                    Text(
+                      recipes[index].description,
+                      style: TextStyle(
+                        fontWeight: FontWeight.w400,
+                        color: Colors.black,
+                        fontSize: 14,
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
+            separatorBuilder: (context, index) => SizedBox(
+              width: 25,
+            ),
+            itemCount: recipes.length,
+            scrollDirection: Axis.vertical,
+            padding: EdgeInsets.only(left: 20, right: 20, bottom: 20),
+          ),
+        )
+      ],
+    );
   }
 }
