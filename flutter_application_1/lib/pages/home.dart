290b77e23c4df468d3f2b50dfa697bb3defe3f35
Christine Bui <kennen0246@gmail.com>
2024-01-15 19:18:57 -0800

    minor sizing adjustments

--- flutter_application_1/lib/pages/home.dart
+++ flutter_application_1/lib/pages/home.dart
@@ -78,11 +78,11 @@ class _HomePage extends State<HomePage> {
           height: 15,
         ),
         SizedBox(
-          height: 340,
+          height: 350,
           child: ListView.separated(
             itemBuilder: (context, index) {
               return Container(
-                width: 250,
+                width: 238,
                 decoration: BoxDecoration(
                   color: Colors.white,
                   borderRadius: BorderRadius.circular(20),
@@ -242,7 +242,7 @@ class _HomePage extends State<HomePage> {
 
   Container searchField() {
     return Container(
-      margin: const EdgeInsets.only(top: 40, left: 20, right: 20),
+      margin: const EdgeInsets.only(top: 20, left: 20, right: 20),
       decoration: BoxDecoration(boxShadow: [
         BoxShadow(
             color: const Color(0xff1D1617).withOpacity(0.11),
