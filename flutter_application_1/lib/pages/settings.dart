eb070d950fd4da0343974f9695247475d143f309
Jeet Patel <68679391+Jeetp0619@users.noreply.github.com>
2024-01-15 16:09:30 -0800

    Update settings.dart

--- flutter_application_1/lib/pages/settings.dart
+++ flutter_application_1/lib/pages/settings.dart
@@ -23,110 +23,111 @@ class _Settings extends State<Settings> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'Settings',
-          ),
-          backgroundColor: Colors.white,
-          elevation: 0,
-          iconTheme: IconThemeData(color: Colors.black),
+      appBar: AppBar(
+        title: Text(
+          'Settings',
         ),
         backgroundColor: Colors.white,
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(left: 20),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'Popular Recipes',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 18,
-                      fontWeight: FontWeight.w600,
-                    ),
+        elevation: 0,
+        iconTheme: IconThemeData(color: Colors.black),
+      ),
+      backgroundColor: Colors.white,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Popular Recipes',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
                   ),
-                  TextButton(
-                    onPressed: () {
-                      // Add your "See All" button action here
-                    },
-                    child: Text(
-                      'See All',
-                      style: TextStyle(
-                        color: Colors.blue, // Change color as needed
-                        fontSize: 14,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(
-              height: 15,
-            ),
-            Container(
-              height: 330,
-              child: ListView.separated(
-                itemBuilder: (context, index) {
-                  return Container(
-                    width: 238,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(20),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black.withOpacity(0.1), // Shadow color
-                          blurRadius: 8, // Blur radius
-                          offset: Offset(3, 6), // Offset in x and y axis
-                        ),
-                      ],
-                    ),
-                    padding: EdgeInsets.all(10),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        ClipRRect(
-                          borderRadius: BorderRadius.circular(20),
-                          child: SizedBox.fromSize(
-                            size: Size.fromRadius(108),
-                            child: Image.asset(
-                              recipes[index].iconPath,
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                        ),
-                        Padding(padding: const EdgeInsets.only(top: 10)),
-                        Text(
-                          recipes[index].name,
-                          style: TextStyle(
-                            fontWeight: FontWeight.w500,
-                            color: Colors.black,
-                            fontSize: 16,
-                          ),
-                        ),
-                        Text(
-                          recipes[index].description,
-                          style: TextStyle(
-                            fontWeight: FontWeight.w400,
-                            color: Colors.black,
-                            fontSize: 14,
-                          ),
-                        ),
-                      ],
-                    ),
-                  );
-                },
-                separatorBuilder: (context, index) => SizedBox(
-                  width: 25,
                 ),
-                itemCount: recipes.length,
-                scrollDirection: Axis.vertical,
-                padding: EdgeInsets.only(left: 20, right: 20, bottom: 20),
+                TextButton(
+                  onPressed: () {
+                    // Add your "See All" button action here
+                  },
+                  child: Text(
+                    'See All',
+                    style: TextStyle(
+                      color: Colors.blue, // Change color as needed
+                      fontSize: 14,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          Expanded(
+            // Wrapping ListView in Expanded
+            child: ListView.separated(
+              itemBuilder: (context, index) {
+                return Container(
+                  width: 238,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.1), // Shadow color
+                        blurRadius: 8, // Blur radius
+                        offset: Offset(3, 6), // Offset in x and y axis
+                      ),
+                    ],
+                  ),
+                  padding: EdgeInsets.all(10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(20),
+                        child: SizedBox.fromSize(
+                          size: Size.fromRadius(108),
+                          child: Image.asset(
+                            recipes[index].iconPath,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                      Padding(padding: const EdgeInsets.only(top: 10)),
+                      Text(
+                        recipes[index].name,
+                        style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          color: Colors.black,
+                          fontSize: 16,
+                        ),
+                      ),
+                      Text(
+                        recipes[index].description,
+                        style: TextStyle(
+                          fontWeight: FontWeight.w400,
+                          color: Colors.black,
+                          fontSize: 14,
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
+              separatorBuilder: (context, index) => SizedBox(
+                width: 25,
               ),
-            )
-          ],
-        ));
+              itemCount: recipes.length,
+              scrollDirection: Axis.vertical,
+              padding: EdgeInsets.only(left: 20, right: 20, bottom: 20),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
