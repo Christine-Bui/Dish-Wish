9ab19ee3595c680ef4d7b4631432949596f929c8
Christine Bui <kennen0246@gmail.com>
2024-01-17 17:29:42 -0800

    Update faq.dart

--- flutter_application_1/lib/pages/faq.dart
+++ flutter_application_1/lib/pages/faq.dart
@@ -3,7 +3,8 @@ import 'package:flutter_application_1/models/recipe_model.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class Faq extends StatefulWidget {
-  const Faq({super.key});
+  const Faq({Key? key}) : super(key: key);
+
   @override
   State<Faq> createState() => _Faq();
 }
@@ -21,10 +22,51 @@ class _Faq extends State<Faq> {
     recipes = RecipeModel.getRecipes();
   }
 
+  Widget buildQuestionContainer(String question, String answer) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.1),
+            blurRadius: 8,
+            offset: const Offset(3, 6),
+          ),
+        ],
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              question,
+              style: const TextStyle(
+                color: Colors.black,
+                fontSize: 14,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            const SizedBox(height: 5),
+            Text(
+              answer,
+              style: const TextStyle(
+                color: Colors.black,
+                fontSize: 14,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white, // Set the background color for the entire screen
+      backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: Colors.white,
         elevation: 0,
@@ -37,54 +79,53 @@ class _Faq extends State<Faq> {
             margin: const EdgeInsets.only(top: 40, left: 20, right: 20),
             decoration: BoxDecoration(boxShadow: [
               BoxShadow(
-                  color: const Color(0xff1D1617).withOpacity(0.11),
-                  blurRadius: 40,
-                  spreadRadius: 0.0)
+                color: const Color(0xff1D1617).withOpacity(0.11),
+                blurRadius: 40,
+                spreadRadius: 0.0,
+              )
             ]),
             child: TextField(
               decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.white,
-                  contentPadding: const EdgeInsets.all(15),
-                  hintText: 'Search',
-                  hintStyle: const TextStyle(color: Color(0xffDDDADA), fontSize: 14),
-                  prefixIcon: Padding(
-                    padding: const EdgeInsets.all(12),
-                    child: SvgPicture.asset('assets/icons/Search.svg'),
-                  ),
-                  suffixIcon: Padding(
-                    padding: const EdgeInsets.all(12),
-                    child: SvgPicture.asset('assets/icons/Filter.svg'),
-                  ),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15),
-                      borderSide: BorderSide.none)),
+                filled: true,
+                fillColor: Colors.white,
+                contentPadding: const EdgeInsets.all(15),
+                hintText: 'Search',
+                hintStyle: const TextStyle(color: Color(0xffDDDADA), fontSize: 14),
+                prefixIcon: Padding(
+                  padding: const EdgeInsets.all(12),
+                  child: SvgPicture.asset('assets/icons/Search.svg'),
+                ),
+                suffixIcon: Padding(
+                  padding: const EdgeInsets.all(12),
+                  child: SvgPicture.asset('assets/icons/Filter.svg'),
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15),
+                  borderSide: BorderSide.none,
+                ),
+              ),
             ),
           ),
-          const SizedBox(height: 20), // Adjust the spacing between the search bar and other content
+          const SizedBox(height: 20),
           Padding(
             padding: const EdgeInsets.only(left: 20),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
+                const Text(
+                  'FAQ',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
                 const Text(
                   'Frequently Asked Questions',
                   style: TextStyle(
                     color: Colors.black,
-                    fontSize: 18,
+                    fontSize: 16,
                     fontWeight: FontWeight.w600,
-                  ),
-                ),
-                TextButton(
-                  onPressed: () {
-                    // Add your "See All" button action here
-                  },
-                  child: const Text(
-                    'See All',
-                    style: TextStyle(
-                      color: Colors.blue, // Change color as needed
-                      fontSize: 14,
-                    ),
                   ),
                 ),
               ],
@@ -94,71 +135,36 @@ class _Faq extends State<Faq> {
             height: 15,
           ),
           Expanded(
-            // Wrapping ListView in Expanded
             child: ListView.separated(
               itemBuilder: (context, index) {
-                return Container(
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(20),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withOpacity(0.1), // Shadow color
-                        blurRadius: 8, // Blur radius
-                        offset: const Offset(3, 6), // Offset in x and y axis
+                return Column(
+                    children: [
+                      buildQuestionContainer(
+                        'How to add to a collection?',
+                        "Navigate to the 'Collections' page by first clicking on the heart icon in the top right of the screen and then locate and click on the plus icon at the bottom right. Enter a name for the collection and press 'ok'.",
                       ),
+                      SizedBox(height: 25),
+                      buildQuestionContainer(
+                        'How to search with ingredients?',
+                        "Click on the filters icon in the search bar at top of the page and click on the option 'by ingredients'.",
+                      ),
+                      SizedBox(height: 25),
+                      buildQuestionContainer(
+                        'How to delete from a collection?',
+                        "Navigate to the 'Collections' page by first clicking on the heart icon in the top right of the screen then long press a collection for a pop-up dialog to appear and select the option to delete.",
+                      ),
+                      SizedBox(height: 25),
+                      buildQuestionContainer(
+                        'How did Stardew Valley influence you?',
+                        "It helped me learn about what to prioritize and how fun the game is.",
+                      ),
+                      SizedBox(height: 20),
                     ],
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(20),
-                    child: Row(
-                      children: [
-                        ClipRRect(
-                          borderRadius: BorderRadius.circular(20),
-                          child: SizedBox.fromSize(
-                            size: const Size.fromRadius(75), // Adjust the image size
-                            child: Image.asset(
-                              recipes[index].iconPath,
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                        ),
-                        const SizedBox(width: 20), // Adjust the spacing between image and text
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                recipes[index].name,
-                                style: const TextStyle(
-                                  fontWeight: FontWeight.w500,
-                                  color: Colors.black,
-                                  fontSize: 16,
-                                ),
-                              ),
-                              const SizedBox(height: 5), // Adjust the spacing between text and description
-                              Text(
-                                recipes[index].description,
-                                style: const TextStyle(
-                                  fontWeight: FontWeight.w400,
-                                  color: Colors.black,
-                                  fontSize: 14,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
                 );
               },
-              separatorBuilder: (context, index) => const SizedBox(
-                height: 25,
-              ),
-              itemCount: recipes.length,
-              scrollDirection: Axis.vertical,
-              padding: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
+              separatorBuilder: (context, index) => SizedBox(height: 25),
+              itemCount: 1,
+              padding: EdgeInsets.only(left: 20, right: 20, bottom: 20),
             ),
           ),
         ],
