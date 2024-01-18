45a4645ed0b9f468e2b7a95324efef39451d70a6
Christine Bui <kennen0246@gmail.com>
2024-01-17 18:19:41 -0800

    Update collection.dart

--- flutter_application_1/lib/pages/collection.dart
+++ flutter_application_1/lib/pages/collection.dart
@@ -39,7 +39,7 @@ class _CollectionPage extends State<CollectionPage> {
 
   Container searchField() {
     return Container(
-      margin: const EdgeInsets.only(top: 40, left: 20, right: 20),
+      margin: const EdgeInsets.only(top: 20, left: 20, right: 20),
       decoration: BoxDecoration(boxShadow: [
         BoxShadow(
             color: const Color(0xff1D1617).withOpacity(0.11),
@@ -68,9 +68,11 @@ class _CollectionPage extends State<CollectionPage> {
     return GridView.builder(
       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 2,
-        crossAxisSpacing: 8.0,
-        mainAxisSpacing: 8.0,
+        crossAxisSpacing: 10.0,
+        mainAxisSpacing: 10.0,
+        childAspectRatio: 1.0,
       ),
+      padding: EdgeInsets.symmetric(horizontal: 20.0),
       itemCount: widget.galleries.length,
       itemBuilder: (context, index) {
         var category = widget.galleries.keys.elementAt(index);
